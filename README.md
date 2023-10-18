0c4846c04377a9f8cc1df88b79753a571cef9a9a
drerx <119292904+drerx@users.noreply.github.com>
2023-10-18 23:14:35 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# apk2url
+
+apk2url easily extracts URL and IP endpoints from an APK file to a .txt output. This is suitable for information gathering by red team, penetration testers and developers to quickly identify endpoints associated with an application.
+
+**Note:** apk2url disassembles to Smali and performs Java decompilation to identify URLs and IPs.
+
+*Inspired by [diggy](https://github.com/s0md3v/Diggy), apk2url is rewritten and upgraded with IP support, stronger regex, filtering and Jadx decompilation.*
+
+## Usage
+```bash
+./apk2url.sh /path/to/apk/file.apk
+```
+
+You can also install directly for easy access by running `./install.sh`.                        
+After that you can run apk2url anywhere:
+```bash
+apk2url /path/to/apk/file.apk
+```
+By default there are 2 output files in the "endpoints" directory:  
+- \<apkname\>_endpoints.txt - **Contains endpoints with url paths, has duplicates**
+- \<apkname\>_uniqurls.txt - **Contains unique endpoints**
+
+By default, the program does not log the path where endpoints are discovered.    
+To enable logging, run as follows:
+
+```bash
+apk2url /path/to/apk/file.apk log
+```
+**Tested on Kali 2023.2 and Ubuntu 22.04*
+
+## Dependencies
+Use `apt` for easy installation of these tools:
+- Apktool
+- Jadx
+
+## Demo Run
+<img width="628" alt="image" src="https://github.com/n0mi1k/apk2url/assets/28621928/7a251297-c1e5-49ba-abe2-6aba67c9fee7">
+
+## Disclaimer
+This tool is for educational and testing purposes only. Do not use it to exploit the vulnerability on any system that you do not own or have permission to test. The authors of this script are not responsible for any misuse or damage caused by its use.
---
+++ apk2url.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+reset='\033[0m'
+
+
+printf """$green       
+ █████╗ ██████╗ ██╗  ██╗██████╗ ██╗   ██╗██████╗ ██╗     
+██╔══██╗██╔══██╗██║ ██╔╝╚════██╗██║   ██║██╔══██╗██║   
+███████║██████╔╝█████╔╝  █████╔╝██║   ██║██████╔╝██║By     
+██╔══██║██╔═══╝ ██╔═██╗ ██╔═══╝ ██║   ██║██╔══██╗██║n0mi1k     
+██║  ██║██║     ██║  ██╗███████╗╚██████╔╝██║  ██║███████╗
+╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝
+$reset"""
+
+BASENAME="$(basename $1 .apk)"
+WORKDIR=`pwd`
+APKPATH="`pwd`/$1"
+DECOMPILEDIR="${WORKDIR}/${BASENAME}-decompiled"
+APKTOOLDIR="${DECOMPILEDIR}/${BASENAME}_apktool"
+JADXDIR="${DECOMPILEDIR}/${BASENAME}_jadx"
+ENDPOINTDIR="${WORKDIR}/endpoints/"
+
+dissectApktool() {
+    printf "$cyan[+] Disassembling ${1}with Apktool...\n$reset"
+    apktool d $APKPATH -o $APKTOOLDIR >/dev/null 2>&1   
+}
+
+dissectJadx() {
+    printf "$cyan[+] Decompiling ${1}with Jadx...\n$reset"
+    jadx $APKPATH -d $JADXDIR>/dev/null 2>&1    
+}
+
+extractEndpoints() {
+    printf "$green[+] Beginning Endpoint Extraction...\n$reset"
+    printf "$yellow[~] Extracting URLs...\n$reset"
+    rawurlmatch=$(grep -rIoE '(\b(https?|ftp|file)://|www\.)[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]' $DECOMPILEDIR)
+    urlmatches=$(printf "%s" "$rawurlmatch" | awk -F':' '{sub(/^[^:]+:/, "", $0); print}' | sort -u)
+    printf "%s\n" "$urlmatches" > "${ENDPOINTDIR}/${BASENAME}_endpoints.txt"
+
+    printf "$yellow[~] Extracting IPs...\n$reset"
+    rawipmatch=$(grep -rIoE '\b((https?|ftp|file)://)?([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}\b' $DECOMPILEDIR)
+    ipmatches=$(printf "%s" "$rawipmatch"| awk -F':' '{sub(/^[^:]+:/, "", $0); print}' | sort -u)    
+    printf "%s\n" "$ipmatches" >> "${ENDPOINTDIR}/${BASENAME}_endpoints.txt"
+
+    if [[ $1 == "log" ]]; then
+        printf "$purple[~] Writing Logs to: ${ENDPOINTDIR}/${BASENAME}_log.txt\n$reset"
+        printf "%s\n" "$rawurlmatch" > "$ENDPOINTDIR/${BASENAME}_log.txt"
+        printf "%s\n" "$rawipmatch" >> "$ENDPOINTDIR/${BASENAME}_log.txt"
+    fi
+
+    printf "$purple[~] Performing Uniq Filter...$reset"
+    grep -oE '((http|https)://[^/]+)' ${ENDPOINTDIR}/${BASENAME}_endpoints.txt | awk -F/ '{print $1 "//" $3}' | sort -u > ${ENDPOINTDIR}/${BASENAME}_uniqurls.txt
+    grep -E '^www.*' ${ENDPOINTDIR}/${BASENAME}_endpoints.txt >> ${ENDPOINTDIR}/${BASENAME}_uniqurls.txt
+    grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*' ${ENDPOINTDIR}/${BASENAME}_endpoints.txt >> ${ENDPOINTDIR}/${BASENAME}_uniqurls.txt
+    
+    printf "$purple\n[~] Wrote Uniq Domains to: ${ENDPOINTDIR}/${BASENAME}_uniqurls.txt\n$reset"
+    printf "$green[*] Endpoints Extracted to: ${ENDPOINTDIR}/${BASENAME}_endpoints.txt\n$reset"
+}
+
+if [[ -z $1 ]]; then
+    printf "Usage: apk2url <Name of APK File>\n"
+    exit 1
+fi
+
+if ! [ -x "$(command -v apktool)" ]; then
+    printf "$red[!] apk2url requires apktool to be installed\n$reset"
+    exit 1
+fi
+
+if ! [ -x "$(command -v jadx)" ]; then
+    printf "$red[!] apk2url requires jadx to be installed$reset"
+    exit 1
+fi
+
+if [ -d "$DECOMPILEDIR" ]; then
+    read -p "[*] APK was decompiled before. Do you want to overwrite it? (y/n): " choice
+    if [[ "${choice,,}" == "y" ]]; then
+        printf "$green[+] Cleaning Up...\n$reset"
+        rm -Rf "$DECOMPILEDIR"
+    else
+        printf "$red[+] Action Aborted...$reset"
+        exit 1
+    fi
+fi
+
+mkdir $DECOMPILEDIR
+
+if [ ! -d "$ENDPOINTDIR" ]; then
+    mkdir "$ENDPOINTDIR"
+fi
+
+printf "$yellow[~] SHA256: $(shasum -a 256 $1 | awk '{print $1}')\n$reset"
+dissectApktool
+dissectJadx
+extractEndpoints $2
